ba60dcc7c2f96780000e15be2a5c3c242a99b198
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-04 06:37:34 +0000

    [Create]管理者商品新規登録画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,10 +1,25 @@
 class Admin::ItemsController < ApplicationController
   def new
+    @item = Item.new
+  end
+
+  def create
+    @store = Store.find(params[:store_id])
+    @item = current_admin_store.items.new(item_params)
+    @item.store_id = @store.id
+    @item.save
+    redirect_to admin_store_item_path(@store.id, @item.id)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:store_id, :category_id, :name, :explanation, :without_tax)
   end
 end
