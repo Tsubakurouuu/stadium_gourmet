fdf67c8a46a34032d341447feea2347798b97db9
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-05 08:11:11 +0000

    [Create]OrderDetailモデルにCartItemを保存

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,6 +8,19 @@ class Public::OrdersController < ApplicationController
     @order.shipping_cost = 200
     @order.order_number = ('A'..'Z').to_a.sample(6)
     @order.save
+    cart_items = CartItem.all
+
+    #カート内商品を保存
+    cart_items.each do |cart_item|
+      order_detail = OrderDetail.new
+      order_detail.item_id = cart_item.item_id
+      order_detail.order_id = @order.id
+      order_detail.purchase_price = cart_item.item.with_tax_price
+      order_detail.amount = cart_item.amount
+      order_detail.save
+    end
+    #保存後カートを空にする
+    cart_items.destroy_all
     redirect_to orders_complete_path
   end
 
