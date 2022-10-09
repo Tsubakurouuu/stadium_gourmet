07ada585ee1068c869de2be711b5ac30364bc7e2
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-09 00:46:17 +0000

    [Add]e-numを使用し注文ステータスを管理

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,4 +8,19 @@ class Admin::OrdersController < ApplicationController
     @order_details = @order.order_details
     @product_total = 0
   end
+
+  def update
+    @order = Order.find(params[:id])
+    
+    # 注文ステータス更新時の処理
+    @order.update(order_params)
+    redirect_to request.referer
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
+
+  enum order_status: {in_production: 0, complete_production: 1, shipping: 2, sent: 3, no_customer: 4}
 end
