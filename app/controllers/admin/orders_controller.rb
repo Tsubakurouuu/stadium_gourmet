eba644332f916de0aa7dda208ed3492d2ae57519
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-10 05:34:24 +0000

    [Fix]管理者側閲覧権限の付与

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,26 +2,20 @@ class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin_store!, only: [:index, :show]
 
   def index
-    @orders = Order.includes(:items).where(items:{ store: current_admin_store})
+    @orders = Order.includes(:items).where(items:{store: current_admin_store})
   end
 
   def show
     @order = Order.find(params[:id])
-    @store = Store.find(params[:id])
-    binding.pry
     @order_details = @order.order_details
     @product_total = 0
     if current_admin_store.owner_flag == true
       return
     end
-    unless @store.id == current_admin_store.id
-      redirect_to admin_store_path(current_admin_store.id)
-    end
   end
 
   def update
     @order = Order.find(params[:id])
-    @store = Store.find(params[:id])
     # 注文ステータス更新時の処理
     @order.update(order_params)
     redirect_to request.referer
