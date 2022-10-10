5b69545d5a096edb7f90083da523917ebb3fce44
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-10 04:02:44 +0000

    [Fix]注文履歴閲覧権限付与

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,8 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin_store!, only: [:index, :show, :update]
+
   def index
+    @store = Order.includes(:items).find_by(items:{ store: current_admin_store})
     @orders = Order.includes(:items).where(items:{ store: current_admin_store})
   end
 
@@ -7,11 +10,18 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details
     @product_total = 0
+    if current_admin_store.owner_flag == true
+      return
+    end
   end
 
   def update
     @order = Order.find(params[:id])
-
+    @store = Store.find(params[:id])
+    if @store.id == current_admin_store.id
+      redirect_to admin_store_path(current_admin_store.id)
+      return
+    end
     # 注文ステータス更新時の処理
     @order.update(order_params)
     redirect_to request.referer
