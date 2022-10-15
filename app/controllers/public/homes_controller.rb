b854a85b0abba256165677446725c0fd41c67ae8
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-15 01:55:57 +0000

    [Update]顧客側トップページレイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
+    @items_new = Item.order(created_at: :desc).limit(4)
+    @items_rank = Item.order(earning: :desc).limit(5)
   end
 end
