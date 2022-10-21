bf1097e3161f0927de9ff9f84b04e414960482fa
Tsubakurouuu <yzpolice2896@icloud.com>
2022-10-21 22:48:31 +0000

    [Fix]管理者閲覧権限付与

--- app/controllers/admin/data_graphs_controller.rb
+++ app/controllers/admin/data_graphs_controller.rb
@@ -1,11 +1,16 @@
 class Admin::DataGraphsController < ApplicationController
   before_action :authenticate_admin_store!, only: [:index]
   def index
-    unless params[:store_id].to_i == current_admin_store.id
-      flash[:alert] = "他の店舗のデータは閲覧できません。"
-      redirect_to admin_store_path(current_admin_store.id)
-      return
+    if current_admin_store.owner_flag == true
+      @store = Store.find(params[:store_id])
+      @items = Item.where(store_id: @store.id)
+    else
+      unless params[:store_id].to_i == current_admin_store.id
+        flash[:alert] = "他の店舗のデータは閲覧できません。"
+        redirect_to admin_store_path(current_admin_store.id)
+        return
+      end
+      @items = Item.where(store_id: current_admin_store.id)
     end
-    @items = Item.where(store_id: current_admin_store.id)
   end
 end
